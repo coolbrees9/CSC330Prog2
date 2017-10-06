ecf9015a6bc1ec8cbcb726d1270a7650da46c050
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-10-06 16:44:23 -0400

    Added nested loops

--- c/diffusion.c
+++ c/diffusion.c
@@ -1,6 +1,8 @@
 //This program will calculate how long it will take to diffuse the size of a room with a substance 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 #define mval(MEM,i,j,k) MEM[i*M*M+j*M+k]
 int main(int argc, char** argv)
 {
@@ -10,8 +12,11 @@ int main(int argc, char** argv)
       int i,j,k,l,m,n;
       double diff = 0.175;
       int urms = 250;
+      clock_t start, end, final;
+      time_t wstart = time(NULL);
+      start = clock();
       double* C = malloc(M*M*M*sizeof(double));
-
+      //Makes the 3D array
       for(i = 0; i < M; i++)
       {
             for(j = 0; j < M; j++)
@@ -22,28 +27,48 @@ int main(int argc, char** argv)
                   }
             }
       }
-      int tstep = (room / urms) / M;
-      double height = room / M;
-      int tacc = 0;
-      double min, max;
+      //More variables 
+      C[0,0,0] = 1.0 * pow(10,21);
+      double tstep = (double) (room / urms) / M;
+      double height = (double) room / M;
+      double tacc = 0.0;
+      double min = mval(C,0,0,1);
+      double max = C[0,0,0];
       double avg = min / max;
+      double dC = C[0,0,1] - C[0,0,0];
+      //Loop that checks if the boxes are not all equal
       while(avg <= 0.99)
       {
             tacc = tacc + tstep;
             for(i = 0; i < M; i++)
             {
-                  for(j = 0; j < M; j++)
+                  for(l = 0; l < M; l++)
                   {
-                        for(k = 0; k < M; k++)
+                        for(j = 0; j < M; j++)
                         {
-                              mval(C,i,j,k) = (diff * (mval(C,l,m,n) - mval(C,i,j,k)) * tstep) / (height * height);
-                              mval(C,i,j,k) = mval(C,i,j,k) + C;
-                              mval(C,l,m,n) = mval(C,l,m,n) - C;
+                              for(m = 0; m < M; m++)
+                              {
+                                    for(k = 0; k < M; k++)
+                                    {
+                                          for(n = 0; n < M; n++)
+                                          {
+                                                mval(C,i,j,k) = (diff * (mval(C,l,m,n) - mval(C,i,j,k)) * tstep) / (height * height);
+                                                mval(C,i,j,k) = mval(C,i,j,k) + (C[M*M*M-1] - dC);
+                                                mval(C,l,m,n) = mval(C,l,m,n) - (C[M*M*M-1] - dC);
+                                                min = C[l,m,n];
+                                          }
+                                    }
+                              }
                         }
                   }
             }
             avg = min / max;
       }
-      //printf("The last array element is %f\n", C[M*M*M-1]);
+      //Calculating the CPU and wall time and printing out
+      end = clock();
+      final = (double) (end - start) / CLOCKS_PER_SEC;
+      printf("CPU time: %.2f\n", final);
+      printf("Wall Clock time: %.2f\n", (double) (time(NULL) - wstart));
+      printf("The last array element is %f\n", C[M*M*M-1]);
       free(C);
 }
