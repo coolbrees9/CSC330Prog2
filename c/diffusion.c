f0c3aaa0c9c3d320d6741e337d4fb0d12e5cb66d
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-10-10 15:03:32 -0400

    Final C

--- c/diffusion.c
+++ c/diffusion.c
@@ -45,9 +45,10 @@ void diffuse(double*** C, int M)
       double height = (double) room / M;
       double tacc = 0.0;
       double ratio = 0.0;
+      double sum = 0.0;
       double dC = (diff * tstep) / (height * height);
       //Loop that checks if the boxes are not all equal
-      while(ratio <= 0.99)
+      while(ratio < 0.99)
       {
             for(int i = 0; i < M; i++)
             {
@@ -63,7 +64,7 @@ void diffuse(double*** C, int M)
                                           {
                                                 //Checks all of the adjacent blocks from the current block
                                                 if(((i==l) && (j==m) && (k==n+1)) || ((i==l) && (j==m) && (k==n-1)) || ((i==l) && (j==m+1) && (k==n)) ||
-                                                   ((i==l) && (j==m-1) && (k==n)) || ((i==l+1) && (j==m) && (k==n)) || ((i==l-1) && (j==m) && (k==n)))
+                                                            ((i==l) && (j==m-1) && (k==n)) || ((i==l+1) && (j==m) && (k==n)) || ((i==l-1) && (j==m) && (k==n)))
                                                 {
                                                       double change = (C[i][j][k] - C[l][m][n]) * dC;
                                                       C[i][j][k] = C[i][j][k] - change;
@@ -77,31 +78,28 @@ void diffuse(double*** C, int M)
             }
             tacc = tacc + tstep;
             //Makes sure there is mass consistency
-            double sum = 0.0;
             double maxc = C[0][0][0];
             double minc = C[0][0][0];
+            sum = 0.0;
             for(int i = 0; i < M; i++)
             {
                   for(int j = 0; j < M; j++)
                   {
                         for(int k = 0; k < M; k++)
                         {
-                              if(maxc < C[i][j][k])
-                                    maxc = C[i][j][k];
-                              //maxc = fmax(C[p][q][r], maxc);
-                              if(minc > C[i][j][k])
-                                    minc = C[i][j][k];
-                              //minc = fmin(C[p][q][r], minc);
+                              maxc = fmax(C[i][j][k], maxc);
+                              minc = fmin(C[i][j][k], minc);
                               sum += C[i][j][k];
-                              //printf("%f  %f\n", minc,maxc);
                         }
                   }
             }
             ratio = minc / maxc;      //Sees if min and max are equal in order to end while loop
             //Prints the different variable types in loop
             /*printf("%f %f %f\n", tacc, ratio, C[0][0][0]);
-            printf("%f\n", C[M-1][M-1][M-1]);
-            printf("%f\n", sum);*/
+              printf("%f\n", C[M-1][M-1][M-1]);
+              printf("%f\n", sum);*/
       }
+      printf("Total sum is %f.\n", sum);
+      //printf("Total time steps are %f.\n", tstep);
       printf("Box completed in %f seconds.\n", tacc);
 }
