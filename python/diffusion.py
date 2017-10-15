f8686b6006b154826f074ea6a511e7abe4ad322d
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-10-15 18:27:07 -0400

    Finished except last value in array is not 1e21

--- python/diffusion.py
+++ python/diffusion.py
@@ -1,39 +1,34 @@
-#Program that will create and diffuse a room with given substance and return the time it takes
 #!/usr/bin/python
-M=10
-room=5
-urms=250
-diff=0.175
-tacc=0
-tstep=(room/urms)/M
-height=room/M
-h2=height*height
-dC=(diff*tstep)/h2
-ratio=0.0
-C = [[[0.0 for k in range(M)]for j in range(M)]for i in range(M)]  #Makes a 3D array based on M dimensions
+#This program will diffuse an entrie room based on its size and M
+import math
 
-#Loop to set every array element to 0
-#for i in range(0,M):
-#      for j in range(0,M):
-#            for k in range(0,M):
-#                  C[i][j][k] = 0.0
-C[0][0][0]=1.0*math.pow(10,21)
+M=3
+room=5.0
+urms=250.0
+diff=0.175
+tacc=0.0
+tstep=(float)((room/urms)/M)
+height=(float)(room/M)
+dC=(float)((diff*tstep)/(height*height))
+ratio=0.0
+C = [[[0.0 for k in range(M)] for j in range(M)] for i in range(M)]  #Makes a 3D array based on M dimensions
+C[0][0][0]=1.0*math.pow(10,21)  #Sets the first element to 1.0e21
 #Loop that will go through entire array and diffuse each cube
 while (ratio <= 0.99):
-      print "Entering while"
       for i in range(0,M):
             for j in range(0,M):
                   for k in range(0,M):
                         for l in range(0,M):
                               for m in range(0,M):
                                     for n in range(0,M):
-                                          if i==l and j==m and k==n+1 or i==l and j==m and k==n-1 or \
-                                             i==l and j==m+1 and k==n or i==l and j==m-1 and k==n or \
+                                          if i==l and j==m and k==n+1 or i==l and j==m and k==n-1 or\
+                                             i==l and j==m+1 and k==n or i==l and j==m-1 and k==n or\
                                              i==l+1 and j==m and k==n or i==l-1 and j==m and k==n:
                                                 change=(C[i][j][k]-C[l][m][n])*dC
                                                 C[i][j][k]=C[i][j][k]-change
                                                 C[l][m][n]=C[l][m][n]+change
       tacc=tacc+tstep
+      #Checks if mass stays consistent
       sumval=0.0
       minval=C[0][0][0]
       maxval=C[0][0][0]
