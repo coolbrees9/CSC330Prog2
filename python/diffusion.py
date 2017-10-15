c6ba26df53918b532cc905acb4e323155ef6f5ea
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-10-15 11:58:23 -0400

    Getting weird syntax error

--- python/diffusion.py
+++ python/diffusion.py
@@ -1,19 +1,50 @@
+#Program that will create and diffuse a room with given substance and return the time it takes
 #!/usr/bin/python
+M=10
+room=5
+urms=250
+diff=0.175
+tacc=0
+tstep=(room/urms)/M
+height=room/M
+h2=height*height
+dC=(diff*tstep)/h2
+ratio=0.0
+C = [[[0.0 for k in range(M)]for j in range(M)]for i in range(M)]  #Makes a 3D array based on M dimensions
 
-M = 10
-room = 5
-urms = 250
-diff = 0.175
-tacc = 0
-tstep = (room / urms) / M
-height = room / M
-
-C = [[[0.0 for k in range(M)] for j in range(M)] for i in range(M)]
-
-for i in range(0, M):
-      for j in range(0, M):
-            for k in range(0, M):
-                  C[i][j][k] = i*M*M + j*M + k + 1
-                  C[i][j][k] = C[i][j][k] * 3.1415926535897
-
-print "Last element is ", C[M-1][M-1][M-1]
+#Loop to set every array element to 0
+#for i in range(0,M):
+#      for j in range(0,M):
+#            for k in range(0,M):
+#                  C[i][j][k] = 0.0
+C[0][0][0]=1.0*math.pow(10,21)
+#Loop that will go through entire array and diffuse each cube
+while (ratio <= 0.99):
+      print "Entering while"
+      for i in range(0,M):
+            for j in range(0,M):
+                  for k in range(0,M):
+                        for l in range(0,M):
+                              for m in range(0,M):
+                                    for n in range(0,M):
+                                          if i==l and j==m and k==n+1 or i==l and j==m and k==n-1 or \
+                                             i==l and j==m+1 and k==n or i==l and j==m-1 and k==n or \
+                                             i==l+1 and j==m and k==n or i==l-1 and j==m and k==n:
+                                                change=(C[i][j][k]-C[l][m][n])*dC
+                                                C[i][j][k]=C[i][j][k]-change
+                                                C[l][m][n]=C[l][m][n]+change
+      tacc=tacc+tstep
+      sumval=0.0
+      minval=C[0][0][0]
+      maxval=C[0][0][0]
+      for i in range(0,M):
+            for j in range(0,M):
+                  for k in range(0,M):
+                        maxval=max(C[i][j][k],maxval)
+                        minval=min(C[i][j][k],minval)
+                        sumval+=C[i][j][k]
+      ratio=minval/maxval
+      print tacc, " ", ratio, " ", C[0][0][0]
+      print C[M-1][M-1][M-1]
+      print sumval
+print "Box diffused in ", tacc, " seconds"
