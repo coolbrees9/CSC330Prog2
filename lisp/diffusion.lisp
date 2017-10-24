9ad19edb434bda6b125bca9df9ab69308dda6817
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-10-24 18:16:39 -0400

    Working on if statement

--- lisp/diffusion.lisp
+++ lisp/diffusion.lisp
@@ -2,7 +2,7 @@
 (defvar C)
 (defvar M 10)
 ;Makes a 3D array
-(setf C (make-array '(M M M)))
+(setf C (make-array '(10 10 10)))
 ;;;Variable declarations
 (defvar diff 0.175)
 (defvar rooms 5.0)
@@ -13,7 +13,7 @@
 (defvar tacc 0.0)
 (defvar cube_ratio 0.0)
 (setf (aref C 0 0 0)(expt 1 21)) ;Makes first element in cube 1e21
-(print "Beginning Box simulation...")
+(write-line "Beginning Box simulation...")
 ;Loop that checks when min and max value are equal
 (loop while (<= cube_ratio  0.99) do
       (dotimes (i M)
@@ -22,12 +22,18 @@
                         (dotimes (l M)
                               (dotimes (m M)
                                     (dotimes (n M)
-                                          (if (and (= i l)(= j m)(= k + n 1))or (and (= i l)(= j m)(= k - n 1))/
-                                           or (and (= i l)(= j + m 1)(= k n))or (and (= i l)(= j - m 1)(= k n))/
-                                           or (and (= i + l 1)(= j m)(= k n))or (and (= i - l 1)(= j m)(= k n))
-                                                (defvar change (* - (aref C i j k)(aref C l m n) dC))
-                                                (setf (aref C i j k)(+ (aref C i j k) change))
-                                                (setf (aref C l m n)(- (aref C l m n) change))
+                                          (if (and (= i l) (= j m) (= k (+ n 1)))
+                                           (or (and (= i l) (= j m) (= k (- n 1))))
+                                           (or (and (= i l) (= j (+ m 1) (= k n))))
+                                           ;(or (and (= i l) (= j (- m 1)) (= k n)))
+                                           ;(or (and (= i (+ l 1)) (= j m) (= k n)))
+                                           ;(or (and (= i (- l 1)) (= j m) (= k n)))
+                                                (progn
+                                                      ;(defvar change (* - (aref C i j k)(aref C l m n) dC))
+                                                      ;(setf (aref C i j k)(+ (aref C i j k) change))
+                                                      ;(setf (aref C l m n)(- (aref C l m n) change))
+                                                      (print "Made it into if statement")
+                                                )
                                           )
                                     )
                               )
@@ -40,19 +46,19 @@
       (defvar sumval 0.0)
       (defvar minval (aref C 0 0 0))
       (defvar maxval (aref C 0 0 0))
-      (dotimes (i M)
-            (dotimes (j M)
-                  (dotimes (k M)
-                        (setf maxval
-                        (setf minval
-                        (setf sumval(+ sumval (aref C i j k)))
-                  )
-            )
-      )
-      (setf cube_ratio(/ minval maxval)
+      ;(dotimes (i M)
+      ;     (dotimes (j M)
+      ;            (dotimes (k M)
+      ;                  (setf maxval (max maxval (aref C i j k)))  ;Determines which is largest of the 2
+      ;                  (setf minval (min minval (aref C i j k)))  ;Determines which is smallest of the 2
+      ;                  (setf sumval(+ sumval (aref C i j k)))
+      ;            )
+      ;      )
+      ;)
+      ;(setf cube_ratio(/ minval maxval)
       ;;;Prints different values
-      (print tacc " " cube_ratio " " (aref C 0 0 0))
-      (print (aref C - M 1 - M 1 - M 1))
-      (print sumval)
+      ;(write-line tacc " " cube_ratio " " (aref C 0 0 0))
+      ;(write-line (aref C - M 1 - M 1 - M 1))
+      ;(write-line sumval)
 )
-(print "Box diffused in " tacc " seconds")
+;(write-line "Box diffused in " tacc " seconds")
