96f6387f46be33d578331049f615d68efc66a9fd
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-11-03 14:38:46 -0400

    Finished C# Code

--- c#/diffusion.cs
+++ c#/diffusion.cs
@@ -15,6 +15,13 @@ public class MemTest
                   Console.WriteLine("Invalid number");
                   System.Environment.Exit(1);
             }
+            Console.WriteLine("Is there a partition? (0 for no, 1 for yes)");
+            int partition = Convert.ToInt32(Console.ReadLine());  //Checks if user wants a partition
+            if(partition != 1 && partition != 0)
+            {
+                  Console.WriteLine("Partition will turn off.");
+                  partition = 0;
+            }
             double[,,]C = new double [M,M,M]; //Creates the 3D array
             //Loop that zeroes out the array
             for(int i = 0; i < M; i++)
@@ -28,12 +35,15 @@ public class MemTest
                   }
             }
             Console.WriteLine("Beginning Diffusion process...");
-            diffuse(C,M);  //Calls the method diffuse
+            diffuse(C,M,partition);  //Calls the method diffuse
       }
       //Method to go through the array and diffuse the box
-      static void diffuse(double[,,] C, int M)
+      static void diffuse(double[,,] C, int M, int partition)
       {
             //More variables
+            decimal partsize = 0;
+            if(partition == 1)
+                  partsize = Decimal.Floor(M / 2);
             C[0,0,0] = 1.0 * Math.Pow(10,21);  //Makes first cell 1.0e21
             double room = 5.0;
             double diff = 0.175;
@@ -44,6 +54,23 @@ public class MemTest
             double ratio = 0.0;
             double sum = 0.0;
             double dC = diff * tstep / (height * height);
+            double change;
+            if(partition == 1)
+            {
+                  for(int i = 0; i < M; i++)
+                  {
+                        for(int j = 0; j < M; j++)
+                        {
+                              for(int k = 0; k < M; k++)
+                              {
+                                    if((i == partsize-1) && (j >= partsize-1))
+                                    {
+                                          C[i,j,k] = -1.0;
+                                    }
+                              }
+                        }
+                  }      
+            }
             //Loop that checks if the boxes are not all equal
             do
             {
@@ -53,24 +80,46 @@ public class MemTest
                         {
                               for(int k = 0; k < M; k++)
                               {
-                                    for(int l = 0; l < M; l++)
+                                    if(C[i,j,k] != -1.0)
                                     {
-                                          for(int m = 0; m < M; m++)
-                                          {
-                                                for(int n = 0; n < M; n++)
+                                                if(i != 0 && C[i-1,j,k] != -1.0)
                                                 {
-                                                      //Checks all of the adjacent blocks from the current block
-                                                      if(((i==l) && (j==m) && (k==n+1)) || ((i==l) && (j==m) && (k==n-1)) || ((i==l) && (j==m+1) && (k==n)) ||
-                                                                  ((i==l) && (j==m-1) && (k==n)) || ((i==l+1) && (j==m) && (k==n)) || ((i==l-1) && (j==m) && (k==n)))
-                                                      {
-                                                            double change = (C[i,j,k] - C[l,m,n]) * dC;
-                                                            C[i,j,k] = C[i,j,k] - change;
-                                                            C[l,m,n] = C[l,m,n] + change;
-                                                      }
+                                                      change=(C[i,j,k]-C[i-1,j,k])*dC;
+                                                      C[i,j,k]=C[i,j,k]-change;
+                                                      C[i-1,j,k]=C[i-1,j,k]+change;
                                                 }
-                                          }
-                                    }
-                              }
+                                                if(j != 0 && C[i,j-1,k] != -1.0)
+                                                {
+                                                      change=(C[i,j,k]-C[i,j-1,k])*dC;
+                                                      C[i,j,k]=C[i,j,k]-change;
+                                                      C[i,j-1,k]=C[i,j-1,k]+change;
+                                                }
+                                                if(k != 0 && C[i,j,k-1] != -1.0)
+                                                {
+                                                      change=(C[i,j,k]-C[i,j,k-1])*dC;
+                                                      C[i,j,k]=C[i,j,k]-change;
+                                                      C[i,j,k-1]=C[i,j,k-1]+change;
+                                                }
+                                                if(i != M-1 && C[i+1,j,k] != -1.0)
+                                                {
+                                                      change=(C[i,j,k]-C[i+1,j,k])*dC;
+                                                      C[i,j,k]=C[i,j,k]-change;
+                                                      C[i+1,j,k]=C[i+1,j,k]+change;
+                                                }
+                                                if(j != M-1 && C[i,j+1,k] != -1.0)
+                                                {
+                                                      change=(C[i,j,k]-C[i,j+1,k])*dC;
+                                                      C[i,j,k]=C[i,j,k]-change;
+                                                      C[i,j+1,k]=C[i,j+1,k]+change;
+                                                }
+                                                if(k != M-1 && C[i,j,k+1] != -1.0)
+                                                {
+                                                      change=(C[i,j,k]-C[i,j,k+1])*dC;
+                                                      C[i,j,k]=C[i,j,k]-change;
+                                                      C[i,j,k+1]=C[i,j,k+1]+change;
+                                                }
+                                    }     
+                              }                       
                         }
                   }
                   tacc = tacc + tstep;
@@ -84,17 +133,20 @@ public class MemTest
                         {
                               for(int k = 0; k < M; k++)
                               {
-                                    maxc = Math.Max(C[i,j,k], maxc);
-                                    minc = Math.Min(C[i,j,k], minc);
-                                    sum += C[i,j,k];
+                                    if(C[i,j,k] != -1.0)
+                                    {
+                                          maxc = Math.Max(C[i,j,k], maxc);
+                                          minc = Math.Min(C[i,j,k], minc);
+                                          sum += C[i,j,k];
+                                    }
                               }
                         }
                   }
                   ratio = minc / maxc;      //Sees if min and max are equal in order to end while loop
                   //Prints the different variable types in loop
-                  /*Console.WriteLine(tacc + " " + ratio + " " + C[0,0,0]);
-                  Console.WriteLine(C[M-1,M-1,M-1]);
-                  Console.WriteLine(sum);*/
+                  Console.WriteLine(tacc + " " + ratio + " " + C[0,0,0]);
+                  //Console.WriteLine(C[M-1,M-1,M-1]);
+                  Console.WriteLine(sum);
             } while(ratio <= 0.99);
             Console.WriteLine("Total sum is " + sum);
             Console.WriteLine("Box completed in " + tacc + " seconds.");
